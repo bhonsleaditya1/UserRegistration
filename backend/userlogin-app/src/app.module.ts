d9c9101864a64328295e43b13a2f65d99c7573b2
Aditya Bhonsle <51963138+bhonsleaditya1@users.noreply.github.com>
2022-02-06 02:12:14 +0530

    Frontend update

--- backend/userlogin-app/src/app.module.ts
+++ backend/userlogin-app/src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { UsersModule } from './modules/users/users.module';
+import { join } from 'node:path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: __dirname+ '/../../front',
+    }),
     DatabaseModule,
     ConfigModule.forRoot({isGlobal:true}),
     UsersModule
